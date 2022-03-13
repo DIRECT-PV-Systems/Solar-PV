4d705e85e809cceb802301ce71c644177842450a
Stephanie Tarczynski <starczyn@uw.edu>
2022-03-12 20:29:31 -0800

    adding more functions to geocoding.py

--- visualizations/geocoding.py
+++ visualizations/geocoding.py
@@ -27,16 +27,79 @@ def find_unique_cities(data):
     unqiue_cities_csv = pd.DataFrame.to_csv(unique_cities, '/home/starczyn/Solar-PV/visualizations/data/unique_cities_sample.csv')
     return unique_cities
 
-def create_geocode_csvs():
+def create_save_files():
     #create the blank csv, only run once
-    #the first csv stores the geocoded data
-    f = open("/home/starczyn/Solar-PV/visualizations/data/TTS_data_geocoded_sample.csv", "w")
+
+    f = open("/home/starczyn/Solar-PV/visualizations/data/TTS_geocoded_sample.csv", "w")
     writer = csv.DictWriter(f, fieldnames= ['geopy location', 'coordinates'])
     writer.writeheader()
     f.close()
-    #second csv stores cities that cannot be geocoded due to error: typos, misspellings, etc.
-    #the user could go back and fix these manually if desired after the code runs once
-    f = open("/home/starczyn/Solar-PV/visualizations/data/TTS_data_error_cities_sample.csv", "w")
+
+    f = open("/home/starczyn/Solar-PV/visualizations/data/TTS_error_cities_sample.csv", "w")
     writer = csv.DictWriter(f, fieldnames= ['location'])
     writer.writeheader()
     f.close()
+
+def geocode_save(row, file = files): #row is pandas series: index in dataframe and value of column at the row
+
+    current_csv = pd.read_csv(file[0], index_col = 0)
+    
+    index = row.name
+    loc = row['city_state_country']
+    
+    #length of csv is number of rows that are done
+    csv_length = len(current_csv) 
+    
+    try: 
+        #if the row has already been geocoded the row is returned back     
+        if index <= csv_length - 1:
+            
+            with open(file[0], 'a') as geopy_csv:
+                append = csv.writer(geopy_csv)
+                geopy_csv.close()
+                
+            return current_csv.iloc[index].values[0]
+
+        #if the row has not been geocoded, the row is geocoded and saved to the csv
+        else:
+            
+            location = geocode(loc)
+
+            with open(file[0], 'a') as geopy_csv:
+                append = csv.writer(geopy_csv)
+                append.writerow(location)
+                geopy_csv.close()
+
+            return "({},{})".format(location.longitude,location.latitude)
+    
+    except:
+
+            with open(file[0], 'a') as geopy_csv:
+                append = csv.writer(geopy_csv)
+                append.writerow(['nan'])
+                geopy_csv.close()
+            
+            with open(file[1], 'a') as error_csv:
+                append = csv.writer(error_csv)
+                append.writerow([loc])
+                error_csv.close()
+
+            return "({},{})".format('nan','nan')
+
+def geocode_unique(unique_cities):
+
+    for idx, row in unique_cities.iterrows():
+        geocode_save(row)
+
+    geopy_df = pd.read_csv('/home/starczyn/Solar-PV/visualizations/data/TTS_geocoded_sample.csv')
+
+    frames = [unique_cities, geopy_df]
+    unique_geo_df = pd.concat(frames, axis=1, join="inner")
+    unqiue_cities_geocoded = pd.DataFrame.to_csv(unique_geo_df, 
+    '/home/starczyn/Solar-PV/visualizations/data/unique_cities_geocoded_sample.csv' )
+
+    return unique_geo_df
+            
+          
+
+        
